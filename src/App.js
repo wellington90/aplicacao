57ab83078efcf66858631ff13a2a622727aba488
w3ll1n9t0n <siswellingtonsantos@gmail.com>
2025-01-28 18:20:51 -0300

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Esse é meu Primeiro App.
+          Esse é meu Primeiro App 
+                  Data 2025.
         </p>
         <a
           className="App-link"
